0fcc9aa82f4bf0626e4b37ff2617bbdcbd345754
Erik Frantz <bardez@bardez.net>
2024-05-19 00:22:31 -0500

    Updating namespaces

--- Bardez.Project.SwordOfTheStars.ResourceManagement/OverrideTech.cs
+++ Bardez.Project.SwordOfTheStars.ResourceManagement/OverrideTech.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
-namespace Bardez.Project.SwordOfTheStars.Editor.UI.WinForms
+namespace Bardez.Project.SwordOfTheStars.ResourceManagement
 {
     public static class OverrideTechList
     {
