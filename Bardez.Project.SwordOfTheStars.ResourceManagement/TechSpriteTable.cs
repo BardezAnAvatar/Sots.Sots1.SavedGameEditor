20933797cc55287a0d78ff06cbf5c5b04c5a0762
Erik Frantz <bardez@bardez.net>
2024-05-20 00:23:31 -0500

    Prune source file down to `TechSpriteTable` class

--- Bardez.Project.SwordOfTheStars.ResourceManagement/TechSpriteTable.cs
+++ Bardez.Project.SwordOfTheStars.ResourceManagement/TechSpriteTable.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 
 namespace Bardez.Project.SwordOfTheStars.ResourceManagement
 {
@@ -79,45 +77,6 @@ namespace Bardez.Project.SwordOfTheStars.ResourceManagement
             }
 
             return word;
-        }
-    }
-
-    public class TechSprite
-    {
-        protected String tech;
-        protected String zipArchivePath;
-
-        public String Tech
-        {
-            get { return tech; }
-            set { tech = value; }
-        }
-
-        public String ZipArchivePath
-        {
-            get { return zipArchivePath; }
-            set { zipArchivePath = value; }
-        }
-
-        public TechSprite()
-        {
-            this.tech = null;
-            this.zipArchivePath = null;
-        }
-
-        public TechSprite(String Name, String Path)
-        {
-            this.tech = Name;
-            this.zipArchivePath = Path;
-        }
-
-        /// <summary>Default for Linq</summary>
-        public static TechSprite Default
-        {
-            get
-            {
-                return new TechSprite();
-            }
         }
     }
 }
