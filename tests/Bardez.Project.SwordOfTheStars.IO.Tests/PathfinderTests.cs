cd8796f04fcfa601afb1fec088a4e5dfd11babfb
Erik Frantz <bardez@bardez.net>
2024-05-20 17:40:53 -0500

    Add in a working unit test

--- tests/Bardez.Project.SwordOfTheStars.IO.Tests/PathfinderTests.cs
+++ tests/Bardez.Project.SwordOfTheStars.IO.Tests/PathfinderTests.cs
@@ -1,4 +1,7 @@
-﻿using FluentAssertions;
+﻿using System.IO;
+using Bardez.Project.SwordOfTheStars.IO.Pathfinding;
+using Bardez.Project.SwordOfTheStars.IO.Pathfinding.Steam;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Bardez.Project.SwordOfTheStars.IO.Tests
@@ -10,13 +13,17 @@ namespace Bardez.Project.SwordOfTheStars.IO.Tests
         public void DeriveSotsPath_Works()
         {
             //arrange
-            var pathfinder = new Pathfinder();
+            var pathfinder = new Pathfinder(new SteamPathfinder(new AcfManifestParserBuilder()));
 
             //act
             var path = pathfinder.DeriveSotsPath();
 
             //assert
-            path.Should().NotBeNull("because the path should be retrieved.");
+            path.Should().NotBeNull("because the path should be retrieved");
+            path.Should().Contain("Sword of the Stars", "because that's a pretty iniversal path for SotS");
+            //TODO: I want to set up different (build?) configurations that have different config values,
+            //  so I can check for a working path on a development machine, and not expect on on test server.
+            Directory.Exists(path).Should().BeTrue("because a dev machine shoulg have the game installed");
         }
     }
 }
