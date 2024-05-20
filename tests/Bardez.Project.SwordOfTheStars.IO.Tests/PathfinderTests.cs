0231d38bac7b833da9075d8c7652b49264452dc1
Erik Frantz <bardez@bardez.net>
2024-05-20 01:44:37 -0500

    Rename the test file

---
+++ tests/Bardez.Project.SwordOfTheStars.IO.Tests/PathfinderTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bardez.Project.SwordOfTheStars.IO.Tests
+{
+    [TestClass]
+    public class PathfinderTests
+    {
+        [TestMethod]
+        public void DeriveSotsPath_Works()
+        {
+            //arrange
+            var pathfinder = new Pathfinder();
+
+            //act
+            var path = pathfinder.DeriveSotsPath();
+
+            //assert
+            path.Should().NotBeNull("because the path should be retrieved.");
+        }
+    }
+}
