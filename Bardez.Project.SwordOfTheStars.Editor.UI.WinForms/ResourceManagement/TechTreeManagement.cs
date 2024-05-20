c50d204a5443755952cd190c1b4cfbef5115d04b
Erik Frantz <bardez@bardez.net>
2024-05-20 00:30:08 -0500

    Prune source file down to `TechTreeManagement` class

--- Bardez.Project.SwordOfTheStars.Editor.UI.WinForms/ResourceManagement/TechTreeManagement.cs
+++ Bardez.Project.SwordOfTheStars.Editor.UI.WinForms/ResourceManagement/TechTreeManagement.cs
@@ -319,33 +319,4 @@ namespace Bardez.Project.SwordOfTheStars.Editor.UI.WinForms.ResourceManagement
             return image;
         }
     }
-
-    public class TreeColumn
-    {
-        protected Int32 group, position;
-
-        public Int32 Group
-        {
-            get { return group; }
-            set { group = value; }
-        }
-
-        public Int32 Position
-        {
-            get { return position; }
-            set { position = value; }
-        }
-
-        /// <summary>Default constructor</summary>
-        public TreeColumn()
-        {
-        }
-
-        /// <summary>Default constructor</summary>
-        public TreeColumn(Int32 Column, Int32 StartPosition)
-        {
-            this.group = Column;
-            this.position = StartPosition;
-        }
-    }
 }
