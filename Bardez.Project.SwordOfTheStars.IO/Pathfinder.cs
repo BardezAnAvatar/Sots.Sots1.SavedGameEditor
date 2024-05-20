328fee1ad66e0e60618bd83f248f187c06772a53
Erik Frantz <bardez@bardez.net>
2024-05-20 01:45:42 -0500

    Adding in a Pathfinder class that should be invoked instead of others

---
+++ Bardez.Project.SwordOfTheStars.IO/Pathfinder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Bardez.Project.SwordOfTheStars.IO.Steam;
+
+namespace Bardez.Project.SwordOfTheStars.IO;
+
+/// <summary>Class that should return the path to the root Sword of the Stars directory</summary>
+public class Pathfinder
+{
+    public String DeriveSotsPath()
+    {
+        //first try to fetch from the registry. Old-school disks install.
+        var sotsPath = RegistryPathfinder.ReadSotsPath();
+        if (string.IsNullOrEmpty(sotsPath))
+        {
+            sotsPath = SteamPathfinder.FindSteamSotsPath();
+        }
+
+        return sotsPath;
+    }
+}
