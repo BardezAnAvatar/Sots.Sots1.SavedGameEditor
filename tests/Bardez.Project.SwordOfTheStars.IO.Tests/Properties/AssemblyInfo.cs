2850cb1523bd71060d4e8c2b861de1c2511898d5
Erik Frantz <bardez@bardez.net>
2024-05-19 20:17:44 -0500

    Adding in a unit test project

---
+++ tests/Bardez.Project.SwordOfTheStars.IO.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bardez.Project.SwordOfTheStars.IO.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bardez.Project.SwordOfTheStars.IO.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b31db891-6bc5-40b7-b9bf-db99f5e9de55")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
