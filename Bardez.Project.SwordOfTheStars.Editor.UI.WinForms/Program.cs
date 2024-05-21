49951a7f7fb582f2b8d4e825c643e9c2919c3db4
Erik Frantz <bardez@bardez.net>
2024-05-21 01:11:17 -0500

    Removing comment

--- Bardez.Project.SwordOfTheStars.Editor.UI.WinForms/Program.cs
+++ Bardez.Project.SwordOfTheStars.Editor.UI.WinForms/Program.cs
@@ -18,7 +18,6 @@ namespace Bardez.Project.SwordOfTheStars.Editor.UI.WinForms
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SotsEditor());
-            //Application.Run(new Form2());
         }
     }
 }
