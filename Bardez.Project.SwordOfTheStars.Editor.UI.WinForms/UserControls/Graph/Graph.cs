ed6dfe68b5b0ad84927e8dad806ade915d04f048
Erik Frantz <bardez@bardez.net>
2024-05-23 23:58:42 -0500

    Allow for horizontal scrolling

--- Bardez.Project.SwordOfTheStars.Editor.UI.WinForms/UserControls/Graph/Graph.cs
+++ Bardez.Project.SwordOfTheStars.Editor.UI.WinForms/UserControls/Graph/Graph.cs
@@ -343,7 +343,16 @@ namespace Bardez.Project.SwordOfTheStars.Editor.UI.WinForms.UserControls.Graph
 
         protected override void OnMouseWheel(MouseEventArgs e)
         {
-            base.OnMouseWheel(e);
+            if (this.VScroll && (Control.ModifierKeys & Keys.Shift) == Keys.Shift)
+            {
+                this.VScroll = false;
+                base.OnMouseWheel(e);
+                this.VScroll = true;
+            }
+            else
+            {
+                base.OnMouseWheel(e);
+            }
             this.Invalidate();
         }
 
